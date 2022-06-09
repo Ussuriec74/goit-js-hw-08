c94922b799a7dee430c17e4001e04ecabf9a87db
Valeriy Semko <valsemko74@gmail.com>
2022-06-09 16:16:02 +0300

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
+import SimpleLightbox from "simplelightbox";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galleryConteinerRef = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+
+galleryConteinerRef.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    `;
+  }).join("");
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
