b1f78192cf6a614a75e83de86e9d162594d30bda
Valeriy Semko <valsemko74@gmail.com>
2022-06-10 17:08:35 +0300

    make video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,13 @@
 import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
+
 
 const iframeRef = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframeRef);
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+const onPlay = function (data) {
+  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(data.seconds));
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
